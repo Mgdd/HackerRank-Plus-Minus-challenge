f01538dbcfc293057063ef3cc93c6cacd7c616df
Elmigdad Sirry <migdad.sirry@gmail.com>
2022-12-05 15:45:21 +0400

    Update Program.cs
    Add comments

--- Program.cs
+++ Program.cs
@@ -19,6 +19,18 @@ class Result
      * Complete the 'plusMinus' function below.
      *
      * The function accepts INTEGER_ARRAY arr as parameter.
+     
+     Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with 6 places after the decimal.
+     Note: This challenge introduces precision problems. The test cases are scaled to six decimal places.
+
+Example
+
+arr=[1,1,0,-1,-1]
+There are n=5 elements, two positive, two negative and one zero. Their ratios are 2/5 = 0.400000, 2/5 = 0.400000 and 1/5 = 0.200000. Results are printed as:
+0.400000
+0.400000
+0.200000
+
      */
 
     public static void plusMinus(List<int> arr)
